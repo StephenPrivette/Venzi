3d153c22f29b51c3fb6d7b68a3b9cdfcc87c5171
imikey30 <imikey30@users.noreply.github.com>
2019-03-19 17:24:19 -0400

    foundations for email functionality

--- CapstoneSolution/CapstoneClassLibrary/EmailLogic.cs
+++ CapstoneSolution/CapstoneClassLibrary/EmailLogic.cs
@@ -8,6 +8,7 @@ namespace CapstoneClassLibrary
 {
     public static class EmailLogic
     {
+        //This will be used to send emails
         public static void SendEmail(string from, List<string> to, string subject, string body)
         {
 
