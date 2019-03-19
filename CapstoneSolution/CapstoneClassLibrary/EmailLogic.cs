0cb453f04303d6fa535eb733d235bb082bb7850d
imikey30 <imikey30@users.noreply.github.com>
2019-03-19 16:51:07 -0400

    email logic class (basics)

---
+++ CapstoneSolution/CapstoneClassLibrary/EmailLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneClassLibrary
+{
+    public static class EmailLogic
+    {
+        public static void SendEmail(string from, List<string> to, string subject, string body)
+        {
+
+        }
+    }
+}
